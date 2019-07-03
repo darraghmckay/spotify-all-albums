7bfa843f7b51f5810c453ddbcd3304e52eb8dcc9
Darragh <darraghmckay@gmail.com>
2019-07-04 00:28:52 +0100

    Updates

---
+++ precache-manifest.cda728939cca09835d9b9eabe5fc5196.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4ef91a2f04cd66d8506000e21720f9",
+    "url": "/spotify-all-albums/index.html"
+  },
+  {
+    "revision": "b466205b3f28b995859a",
+    "url": "/spotify-all-albums/static/css/main.4a7c63af.chunk.css"
+  },
+  {
+    "revision": "0f73e962ce92816cf22f",
+    "url": "/spotify-all-albums/static/js/2.2d7ca78f.chunk.js"
+  },
+  {
+    "revision": "b466205b3f28b995859a",
+    "url": "/spotify-all-albums/static/js/main.9fc3f00a.chunk.js"
+  },
+  {
+    "revision": "b0e1b35db80f4d982763",
+    "url": "/spotify-all-albums/static/js/runtime~main.7fd17c67.js"
+  }
+]);
